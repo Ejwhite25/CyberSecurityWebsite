c03463cf6576f9fade8928879aab37bbe3e3d752
Ejwhite25 <evanjwhite2599@gmail.com>
2022-02-12 14:25:56 -0700

    XAMPP AND Github integration finally working

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo '<h1>a test commit 2</h1>';
+echo '<h1>a test commit 3</h1>';
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
