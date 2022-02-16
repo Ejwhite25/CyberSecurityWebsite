e8e4a919a1a21b88e4cb9c08af8c81f408954686
Ejwhite25 <evanjwhite2599@gmail.com>
2022-02-16 11:34:22 -0700

    Added information for website

--- index.php
+++ index.php
@@ -1,5 +1,80 @@
 <?php
-echo '<h1>a test commit 3</h1>';
+echo '<h1>Overview</h1>
+<p>The Information Security emphasis (also referred to as “Cybersecurity”) 
+will introduce you to principles and concepts of information security and hacking. 
+Coursework uses real-world examples to illustrate attacks on computer systems and networks and teaches you basic hacking concepts with defensive measures. As a computer scientist, you’ll also need to be more than technically proficient. Reading, writing, critical analysis and presentation skills are essential for those who wish to do more than develop software. 
+The dynamic blend of Western’s liberal arts and computer science courses will provide you with the hard and soft skills necessary to manage people and projects.
+
+To start building your resume, you can apply for competitive, paid internships with Lockheed Martin, Amazon, Northrup Grumman and more. 
+For opportunities on-campus, you can work for the Western IT Department and help protect the university’s cyber network. 
+Due to the increasing demand for this skillset, you’ll enter the workforce prepared for challenging work and great career opportunities.</p>
+<h1>Program Requirements</h1>
+<p> Core of Computer Science</p>
+<table>
+<thead>
+<tr>
+<th>Code</th>
+<th>Class Name</th>
+<th>Credits</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>CS 190</td>
+<td>Computer Science I</td>
+<td>3</td>
+</tr>
+<tr>
+<td>CS 191</td>
+<td>Computer Science II</td>
+<td>3</td>
+</tr>
+<tr>
+<td>CS 195</td>
+<td>Database Management System</td>
+<td>3</td>
+</tr>
+<tr>
+<td>CS 250</td>
+<td>Web Application Development I</td>
+<td>3</td>
+</tr>
+<tr>
+<td>CS 280</td>
+<td>Data Structures</td>
+<td>3</td>
+</tr>
+<tr>
+<td>CS 330</td>
+<td>Operating systems and architecture</td>
+<td>3</td>
+</tr>
+<tr>
+<td>CS 370</td>
+<td>Systems Programming in C</td>
+<td>3</td>
+</tr>
+<tr>
+<td>CS 412</td>
+<td>Software Engineering</td>
+<td>3</td>
+</tr>
+<tr>
+<td>CS 470</td>
+<td>Algorithms</td>
+<td>3</td>
+</tr>
+<tr>
+<td>CS 495</td>
+<td>Senior Project</td>
+<td>3</td>
+</tr>
+<tr>
+<td><h2>Total Credits</h2></td>
+<td><h2>30</h2></td>
+</tr>
+</tbody>
+</table>';
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
