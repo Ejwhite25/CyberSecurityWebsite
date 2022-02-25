c03af59fbea8d2b4b8a81d92f867c1aebbec5361
Ejwhite25 <evanjwhite2599@gmail.com>
2022-02-25 11:43:49 -0700

    Degree plan information for first year, and added a column for professors teaching the course in the program requirements table

--- index.php
+++ index.php
@@ -8,6 +8,16 @@ The dynamic blend of Western’s liberal arts and computer science courses will 
 To start building your resume, you can apply for competitive, paid internships with Lockheed Martin, Amazon, Northrup Grumman and more. 
 For opportunities on-campus, you can work for the Western IT Department and help protect the university’s cyber network. 
 Due to the increasing demand for this skillset, you’ll enter the workforce prepared for challenging work and great career opportunities.</p>
+<style>
+table {
+  border-spacing: 30px;
+}
+</style>
+<h1>Professors</h1>
+<ul>
+<li><a href="https://western.edu/people/doug-macgregor-phd/">Doug Macgregor</a></li>
+<li><a href="https://western.edu/people/marc-rubin-phd/">Marc Rubin</a></li>
+</ul>
 <h1> Program Certifications</h1>
 <p>NSA CAE-CD Certified</p>
  <img src = "https://cyber.tstc.edu/wp-content/uploads/2018/09/Seals-1.jpg"
@@ -21,6 +31,7 @@ Due to the increasing demand for this skillset, you’ll enter the workforce pre
 <th>Code</th>
 <th>Class Name</th>
 <th>Credits</th>
+<th>Professor</th>
 </tr>
 </thead>
 <tbody>
@@ -28,6 +39,7 @@ Due to the increasing demand for this skillset, you’ll enter the workforce pre
 <td>CS 190</td>
 <td>Computer Science I</td>
 <td>3</td>
+<td><a href="https://western.edu/people/marc-rubin-phd/">Marc Rubin</a></td>
 </tr>
 <tr>
 <td>CS 191</td>
@@ -43,6 +55,7 @@ Due to the increasing demand for this skillset, you’ll enter the workforce pre
 <td>CS 250</td>
 <td>Web Application Development I</td>
 <td>3</td>
+<td><a href="https://western.edu/people/marc-rubin-phd/">Marc Rubin</a></td>
 </tr>
 <tr>
 <td>CS 280</td>
@@ -53,35 +66,42 @@ Due to the increasing demand for this skillset, you’ll enter the workforce pre
 <td>CS 330</td>
 <td>Operating systems and architecture</td>
 <td>3</td>
+<td><a href="https://western.edu/people/doug-macgregor-phd/">Doug Macgregor</a></td>
 </tr>
 <tr>
 <td>CS 370</td>
 <td>Systems Programming in C</td>
 <td>3</td>
+<td><a href="https://western.edu/people/marc-rubin-phd/">Marc Rubin</a></td>
 </tr>
 <tr>
 <td>CS 412</td>
 <td>Software Engineering</td>
 <td>3</td>
+<td><a href="https://western.edu/people/marc-rubin-phd/">Marc Rubin</a></td>
 </tr>
 <tr>
 <td>CS 470</td>
 <td>Algorithms</td>
 <td>3</td>
+<td>Sam Micka</td>
 </tr>
 <tr>
 <td>CS 495</td>
 <td>Senior Project</td>
 <td>3</td>
+<td><a href="https://western.edu/people/marc-rubin-phd/">Marc Rubin</a></td>
 </tr>
 <tr>
 <td><h2>Total Credits</h2></td>
+<td>                      </td>
 <td><h2>30</h2></td>
 </tr>
 <tr>
 <td>CS 170</td>
 <td>Information Security and Hacking</td>
 <td>3</td>	
+<td><a href="https://western.edu/people/doug-macgregor-phd/">Doug Macgregor</a></td>
 </tr>
 <tr>
 <td>CS 235</td>	
@@ -92,23 +112,31 @@ Due to the increasing demand for this skillset, you’ll enter the workforce pre
 <td>CS 360</td>
 <td>System Security</td>
 <td>3</td>
+<td><a href="https://western.edu/people/doug-macgregor-phd/">Doug Macgregor</a></td>
 </tr>
 <tr>
 <td>CS 450</td>	
 <td>Ethical Hacking and Malware</td>
 <td>3</td>
+<td><a href="https://western.edu/people/doug-macgregor-phd/">Doug Macgregor</a></td>
 </tr>
 <tr>
 <td>CS 460</td>	
 <td>Network Security</td>	
 <td>3</td>
+<td><a href="https://western.edu/people/doug-macgregor-phd/">Doug Macgregor</a></td>
 </tr>
 <tr>
 <td>MAT 140</td>
 <td>College Algebra</td>
 <td>3</td>
 </tr>
+<tr>
+<td><h2>Total Credits</h2></td>
+<td>                      </td>
+<td><h2>18</h2></td>
 </tbody>
+</tr>
 </table>
 <colgroup>
 <col class="codecol">
@@ -116,8 +144,95 @@ Due to the increasing demand for this skillset, you’ll enter the workforce pre
 <col class="hourscol">
 </colgroup>
 <h1>Degree Plan</h1>
-
-
+<table>
+<tr>
+<thead>Year One</thead>
+</tr>
+<tr>
+<th>Fall</th>
+<th>       </th>
+<th>Credits</th>
+</tr>
+<tbody>
+<tr>
+<td>CS 170</td>
+<td>Information Security and Hacking</td>
+<td>3</td>
+</tr>
+<tr>
+<td>CS 190</td>
+<td>Computer Science I</td>
+<td>3</td>
+</tr>
+<tr>
+<td>ENG 102</td>
+<td>Academic Writing (GT_CO1)</td>
+<td>3</td>
+</tr>
+<tr>
+<td>HWTR 100</td>
+<td>First year seminar</td>
+<td>1</td>
+</tr>
+<tr>
+<td>MATH 140
+or MATH 141
+or MATH 151
+</td>
+<td>
+College Algebra (GT-MA1)
+or Precalculus (GT-MA1)
+or Calculus I (GT-MA1)
+</td>
+<td>3</td>
+</tr>
+<tr>
+<td>Gen Ed</td>
+<td>Arts & Humanities</td>
+<td>3</td>
+</tr>
+<tr>
+<td>         </td>
+<td style="font-weight:bold">Credits</td>
+<td>16</td>
+</tr>
+<tr>
+<th>Spring</th>
+<th>      </th>
+<th>      </th
+</tr>
+<tr>
+<td>CS 191</td>
+<td>Computer Science II</td>
+<td>3</td>
+</tr>
+<tr>
+<td>CS 195</td>
+<td>Database Management Systems</td>
+<td>3</td>
+</tr>
+<tr>
+<td>Elective</td>
+<td>Elective or minor course</td>
+<td>3</td>
+</tr>
+<tr>
+<td>Gen Ed</td>
+<td>Social Sciences</td>
+<td>3</td>
+</tr>
+<tr>
+<td>Gen Ed</td>
+<td>Arts and humanities</td>
+<td>3</td>
+</tr>
+<tr>
+<td>         </td>
+<td style="font-weight:bold">Credits</td>
+<td>15</td>
+</tr>
+</tbody>
+</table>
 <h1>Contact Us</h1>
 <p>Contact either Marc or Doug for any questions or speaking opportunities!</p>
 <a href="ContactForm.html">Contact Form</a>
